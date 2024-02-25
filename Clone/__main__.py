eb47d0796f113da7aeb0f922f8fbf6676bc00113
Vishal-Borase <126957307+Vishal-1756@users.noreply.github.com>
2024-02-25 22:13:05 +0530

    Update __main__.py

--- Clone/__main__.py
+++ Clone/__main__.py
@@ -1,8 +1,4 @@
-import pyrogram
 from Clone import bot
-
-async def run_clients():
-      await bot.start()
       
 if __name__ == "__main__":
-    bot.loop.run_until_complete(run_clients())
+      bot.start()
