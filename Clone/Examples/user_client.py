99fb40a140726928111f227ee53da43f1f2f55f9
Vishal-Borase <126957307+Vishal-1756@users.noreply.github.com>
2024-06-02 17:27:43 +0530

    Update user_client.py

--- Clone/Examples/user_client.py
+++ Clone/Examples/user_client.py
@@ -27,3 +27,37 @@ async def user_clone(bot: bot, msg: Message):
         await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Use Help For Help Menu\n\nThanks for Cloning.\n **ignore this message its happened due to over load**")
     except Exception as e:
         await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+
+
+import asyncio
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Clone import bot
+from Clone import API_ID, API_HASH
+import pyrogram
+
+@bot.on_message(filters.private & filters.command("uclone"))
+async def bot_clone(bot: Client, msg: Message):
+    chat = msg.chat
+    cmd = msg.command
+    try:
+        SESSION = msg.text.split()[1]
+    except IndexError:
+        await msg.reply("Please provide a valid BOT_TOKEN.\nUsage:\n\n /uclone STRING SESSION")
+        return
+
+    text = await msg.reply("Booting Your Client")
+    
+    async def start_new_client():
+        try:
+            client = Client(":memory:", API_ID, API_HASH, session_string=SESSION, plugins={"root": "Clone"})
+            await client.start()
+            user = await client.get_me()
+            await text.edit(f"Booted Client as @{user.username} Do /ping for testing")        
+            await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Use Help For Help Menu\n\nThanks for Cloning.\n **ignore this message its happened due to over load**")
+            await pyrogram.idle()
+        except Exception as e:
+            await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+
+    asyncio.create_task(start_new_client())
