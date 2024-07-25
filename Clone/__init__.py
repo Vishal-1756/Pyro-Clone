12f11986a13840adf2de0013d3e3efe1e1b414ff
Vishal-Borase <126957307+Vishal-1756@users.noreply.github.com>
2024-07-25 19:49:21 +0530

    Update __init__.py

--- Clone/__init__.py
+++ Clone/__init__.py
@@ -17,9 +17,9 @@ StartTime = time.time()
 
 
 # Set up bot configurations
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-TOKEN = os.getenv("TOKEN")
+API_ID = 25629197
+API_HASH = "fd41f8bacda97ab0a3ad120b30339978"
+TOKEN = "6528046179:AAHkcHbalYrhZjD4rjDXVagtYY1hiSK34zE"
 
 #PYROGRAM BOT CLIENT
 bot = Client(name="CloneBot", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Clone.Examples"))
